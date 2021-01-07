e0bbeb8083975181f31e62326062fc66125bbbc1
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-01-07 17:17:30 +0700

    das

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" type="text/css" href="{{asset('css/styles.css')}}">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
-  <script src="{{asset('resources/js/bootstrap/jquery-3.5.1.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap/jquery-3.5.1.min.js')}}"></script>
 
 </head>
 <body>
