b92c75ee8719509564d33ea3edd2c6d680ad4d8d
cuong <nqcuong.cnq@gmail.com>
2021-01-19 08:38:22 +0700

    nbkbhk

--- app/Containers/AccountManager/Actions/GetAllAccountManagersAction.php
+++ app/Containers/AccountManager/Actions/GetAllAccountManagersAction.php
@@ -1,5 +1,23 @@
 <?php
-
+/**
+ * @todo action cập nhật AccountManager từ CSDL.
+ * @author Hoàng
+ * @input 
+ * id : để tìm đúng dữ liệu cần cập nhật
+ * DataTransporter $params
+ * - company_name : Tên công ty
+ * - app_name : Tên ứng dụng
+ * - app_brand : Loại ứng dụng ENUM('PMVE','WEBSITE_PMVE','PMHANG','TRACKING_PMHANG','NHAN_SU','MANUAL')
+ * - app_code : Mã ứng dụng
+ * - domain_name : Tên domain
+ * - site_name : Tên site
+ * - base_url : Địa chỉ url
+ * @output: entity AccountManager vừa cập nhật
+ * @return App\Containers\AccountManager\Models\AccountManager AccountManager vừa cập nhật
+ * @algorithm
+ * _ Action được gọi bởi controller
+ * _ GetAllAccountManagersAction gọi đến task GetAllAccountManagersTask để xử lý nghiệp vụ cập nhật AccountManager
+ */
 namespace App\Containers\AccountManager\Actions;
 
 use App\Ship\Parents\Actions\Action;
